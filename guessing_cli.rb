fa34d8c9ea946feb168997e06ea36b09cfe58edd
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-27 23:42:37 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -12,12 +12,13 @@ while user_input != "exit" do
   user_input = gets.chomp
   if user_input.to_i == random_num
     puts "You guessed the correct number!"
-    break
-  else 
+    
+  elsif user_input != random_num
     puts "The computer guessed #{random_num}."
-    break
+  else 
+    puts "Invalid input"
+    
   end
 end
-  puts "Goodbye!"
   
 end
