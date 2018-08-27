4a8b9b25946eae5ee05b8f6c8aca7687fff7cb0a
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-27 23:28:50 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -12,7 +12,7 @@ if user_input == "exit"
   
 elsif user_input.to_i == random_num
     puts "You guessed the correct number!"
-  else user_input.to_i != random_num
+  else 
     puts "The computer guessed #{random_num}."
   end
 end
